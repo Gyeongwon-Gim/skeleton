fac17bab3a7869d267ada10f180fc8d14b4557bc
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-10-08 18:43:59 +0900

    Feat: 백틱으로 감싸는 유틸 생성
    - wrapBacktick() : 식별자를 점(.)으로 구분하여 백틱으로 감싸는 함수
    - wrapBacktickExpression() : 표현식(연산자 + 피연산자) 꼴의 문자열에서 식별자를 구분하여 백틱으로 감싸는 함수

---
+++ src/utills/utill.js
@@ -0,0 +1,32 @@
+/**
+ * "books.category_id" -> "\`books\`.\`category_id\`"
+ * @param {string} identifiers
+ * @returns {string}
+ */
+export function wrapBacktick(identifiers) {
+    // identifiers가 객체일 경우 { key : value } => key
+    return identifiers
+        .split(".")
+        .map((id) => `\`${id}\``)
+        .join(".");
+}
+
+/**
+ * "books.category_id = category.id" -> "\`books\`.\`category_id\` = \`category\`.\`id\`"
+ * @param {string} expression
+ * @returns {string}
+ */
+export function wrapBacktickExpression(expression) {
+    const regex = /(=|>|<|>=|<=|!=|\+|-|\*|\/|AND|OR|\|\||&&|LIKE)/;
+    // 정규식을 이용해 연산자를 기준으로 분리
+    return expression
+        .split(regex)
+        .map((token) => {
+            // 토큰이 연산자가 아닐 경우
+            if (!regex.test(token)) {
+                return wrapBacktick(token.trim());
+            }
+            return token;
+        })
+        .join(" ");
+}
