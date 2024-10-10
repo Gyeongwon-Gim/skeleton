d9ba1af4fbb6e5848f43ea5127397fdae768708d
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-10-10 18:42:39 +0900

    Fix: 별표(*) 구분
    MySQL 에서 별표(*)는 '곱하기' 와 '모든 컬럼' 을 의미하는데, 이를 구분해주도록 수정

--- src/test/backtick.test.js
+++ src/test/backtick.test.js
@@ -4,13 +4,18 @@ function wrapBacktickTest() {}
 
 function wrapBacktickExpressionTest() {
     // given
-    const inputs = ["price > 10", "name LIKE Herry Potter"];
+    const inputs = ["price > 10", "name LIKE 'Herry Potter'", "COUNT(*) > 1"];
 
     // when
     const results = inputs.map((input) => wrapBacktickExpression(input));
 
     // then
-    const expected = ["`price` > 10", "name LIKE 'Herry Potter'"];
+    const expected = ["`price` > 10", "`name` LIKE 'Herry Potter'", "COUNT(*) > 1"];
+
+    // 결과와 기대값 출력
+    console.log("Results:", results);
+    console.log("Expected:", expected);
+
     const isPass = results.every((result, i) => result === expected[i]);
     console.log(`wrapBacktickExpression : ${isPass}`);
 }
--- src/utils/backtick.js
+++ src/utils/backtick.js
@@ -13,24 +13,42 @@ export function wrapBacktick(identifiers) {
         .join(".");
 }
 
-
 /**
  * "books.category_id = category.id" -> "\`books\`.\`category_id\` = \`category\`.\`id\`"
  * @param {string} expression
  * @returns {string}
  */
 export function wrapBacktickExpression(expression) {
-    const operators = /(=|>|<|>=|<=|!=|\+|-|\*|\/|\bAND\b|\bOR\b|\|\||&&|\bLIKE\b|\bBETWEEN\b)/i;
-    const string = /''/;
+    const operators = /(=|>|<|>=|<=|!=|\+|-|\/|\bAND\b|\bOR\b|\|\||&&|\bLIKE\b|\bBETWEEN\b)/i;
+    const stringLiteral = /['"].*['"]/; // 작은따옴표 또는 큰따옴표로 감싸진 문자열
+    const asteriskInFunction = /\b\w+\(\s*\*\s*\)/; // 함수 안에 '*'가 있는 패턴
+    const multiplyAsterisk = /\*/; // 곱하기 연산자
+
+    const tokens = expression.match(/(\w+\(\s*\*\s*\)|\*|['"].*['"]|[^\s]+|[\s]+)/g); // (함수 안의 * | 그냥 * | 따옴표로 감싸진 문자열 | 문자열 | 공백)을 기준으로 토큰화
     // 정규식을 이용해 연산자를 기준으로 분리
-    return expression
-        .split(operators)
+    return tokens
         .map((token) => {
-            // 토큰이 연산자가 아닐 경우
-            if (operators.test(token)) {
+            // 함수 안의 *인 경우 그대로 반환
+            if (asteriskInFunction.test(token)) {
                 return token;
             }
+
+            // 토큰이 연산자인 경우 그대로 반환
+            if (operators.test(token) || multiplyAsterisk.test(token)) {
+                return token;
+            }
+
+            // 토큰이 문자열 리터럴일 경우 백틱으로 감싸지 않음
+            if (stringLiteral.test(token)) {
+                return token;
+            }
+
+            // 토큰이 space 문자일 경우 빈 문자열 반환
+            if (token === " ") {
+                return " ";
+            }
+            // 연산자가 아닌 경우 백틱 처리
             return wrapBacktick(token.trim());
         })
-        .join(" ");
+        .join("");
 }
