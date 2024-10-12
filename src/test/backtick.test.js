fd14fbf6639143038df32bae0001e0c61bdb7278
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-10-12 16:48:46 +0900

    Refactor: 백틱 모듈 리팩토링

--- src/test/backtick.test.js
+++ src/test/backtick.test.js
@@ -1,16 +1,41 @@
-import { wrapBacktick, wrapBacktickExpression } from "../utils/backtick.js";
+import { wrapIdentifier, wrapBacktickExpression, wrapCols } from "../utils/backtick.js";
 
-function wrapBacktickTest() {}
+function wrapIdentifierTest() {
+    // given
+    const inputs = ["t1.col1", "db1.t1.col1", "col1", "t1.*"];
+
+    // when
+    const results = inputs.map((input) => wrapIdentifier(input));
+
+    // then
+    const expected = ["`t1`.`col1`", "`db1`.`t1`.`col1`", "`col1`", "`t1`.*"];
+    const isPass = results.every((result, i) => {
+        if (result instanceof TypeError) {
+            return result.message === expected[i].message;
+        } else {
+            return result === expected[i];
+        }
+    });
+    console.log(`wrapIdentifier : ${isPass}`);
+}
 
 function wrapBacktickExpressionTest() {
     // given
-    const inputs = ["price > 10", "name LIKE 'Herry Potter'", "COUNT(*) > 1"];
+    const inputs = [
+        "books.category_id = category.id AND books.price > 10",
+        "books.title LIKE 'Harry Potter'",
+        "COUNT(CONCAT(t1.name, t2.name))",
+    ];
 
     // when
     const results = inputs.map((input) => wrapBacktickExpression(input));
 
     // then
-    const expected = ["`price` > 10", "`name` LIKE 'Herry Potter'", "COUNT(*) > 1"];
+    const expected = [
+        "`books`.`category_id` = `category`.`id` AND `books`.`price` > 10",
+        "`books`.`title` LIKE 'Harry Potter'",
+        "COUNT(CONCAT(`t1`.`name`, `t2`.`name`))",
+    ];
 
     // 결과와 기대값 출력
     console.log("Results:", results);
@@ -20,4 +45,6 @@ function wrapBacktickExpressionTest() {
     console.log(`wrapBacktickExpression : ${isPass}`);
 }
 
+wrapIdentifierTest();
+wrapColsTest();
 wrapBacktickExpressionTest();
