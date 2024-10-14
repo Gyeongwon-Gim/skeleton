5f6357aa226012ce42cdd09e7b55b4cf9da170aa
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-10-14 22:33:09 +0900

    Fix: 정규식 수정
    예약어를 따로 작성해서 구분해줌.

--- src/utils/backtick.js
+++ src/utils/backtick.js
@@ -1,8 +1,10 @@
 const pattern = {
     operators: /(\bAND\b|\bOR\b|\bLIKE\b|\bBETWEEN\b)/i,
-    identifier: /(\w+\s*\w+)/g,
+    reserved:
+        /(\bSELECT\b|\bDELETE\b|\bUPDATE\b|\bINSERT\b|\bFROM\b|\bWHERE\b|\bSET\b|\bVALUES\b|\bJOIN\b|\bORDER BY\b|\bLIMIT\b|\bINTO\b|\bHAVING\b|\bGROUP BY\b|\bDISTINCT\b|\bAS\b)/i,
+    identifier: /(\w+)/g,
     digit: /\b\d+\b/,
-    string: /('\s*.*?\s*'|"\s*.*?\s*")/,
+    string: /('\s*.*?\s*'|"\s*.*?\s*"|`\s*.*?\s*`)/,
     func: /\w+\((\s*.*\s*)\)/,
 };
 
@@ -22,10 +24,11 @@ export function recurseFuncBracket(func) {
  */
 export function wrapBacktick(expression) {
     return expression.replace(
-        /('\s*.*?\s*'|"\s*.*?\s*"|\bAND\b|\bOR\b|\bLIKE\b|\bBETWEEN\b|\w+\(\s*.*\s*\)|\d+|\w+(\s*\w+|\.\w+))/g,
+        /('\s*.*?\s*'|"\s*.*?\s*"|`\s*.*?\s*`|\w+\(\s*.*\s*\)|\d+|\w+)/g,
         (match, p1) => {
             if (pattern.string.test(p1)) return p1;
             else if (pattern.operators.test(p1)) return p1;
+            else if (pattern.reserved.test(p1)) return p1;
             else if (pattern.func.test(p1)) return recurseFuncBracket(p1);
             else if (pattern.digit.test(p1)) return p1;
             else return wrapIdentifier(p1);
