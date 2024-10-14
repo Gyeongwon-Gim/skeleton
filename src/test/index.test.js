b31747e3dbf857f055d2ca9af87b3c6b6cd60279
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-10-14 14:24:28 +0900

    Test: 실제 사용자 사용 예시

---
+++ src/test/index.test.js
@@ -0,0 +1,29 @@
+import skeleton, { TYPE, JOIN } from "../skeleton.js";
+import express from "express";
+import mysql from "mysql2/promise";
+
+const conn = await mysql.createConnection({
+    host: "localhost",
+    password: "root",
+    user: "root",
+    database: "BookStore",
+    dateStrings: true,
+});
+const mariadb = new skeleton.QueryExecutor(conn);
+
+const app = express();
+app.listen(3000);
+
+const sql = {
+    type: TYPE.SELECT,
+    cols: [],
+    from: ["books"],
+};
+
+const fetchAllBooks = async (req, res) => {
+    const [results] = await mariadb.query(sql);
+    console.log(results);
+    res.json(results);
+};
+
+app.get("/books", fetchAllBooks);
